854a74b65b8b76e877e40c9b8c9c3ca87f4262de
aaqure786 <88874833+aaqure786@users.noreply.github.com>
2022-04-17 01:52:08 -0700

    Connection to Mongo db and making some routes
    Adding data in mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,14 @@
-const connectToMong = require('./db');
+const connectToMong =require('./db.js');
 const express = require('express');
+
+
  
 connectToMong(); 
 const app = express()
 const port = 3000
+app.use(express.json())
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

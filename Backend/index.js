e6d31938d9d72408456612e0d1daa772ec5308ed
aaqure786 <88874833+aaqure786@users.noreply.github.com>
2022-04-17 12:36:51 -0700

    Added hashing + salt concept in password securing

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,9 @@ const express = require('express');
  
 connectToMong(); 
 const app = express()
-const port = 3000
+const port = 5000
 app.use(express.json())
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth/', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,24 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/Users')
-const { body, validationResult } = require('express-validator');
+const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
-// Create a User using POST "/api/auth/". Does not require auth
-router.post('/',[
-    body('name',"Enter vlaid Name").isLength({min: 3}),
-    body('email',"Enter valid Email").isEmail(),
-    body('password',"Password must be greater than 5 character").isLength({min:5})
-], 
-(req, res)=> {
+// Create a User using POST "/api/auth/registeruser".
+router.post(
+  "/registeruser",
+  [
+    body("name", "Enter vlaid Name").isLength({ min: 3 }),
+    body("email", "Enter valid Email").isEmail(),
+    body("password", "Password must be greater than 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //Checking validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // checking weather user with the email is already exists or not
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User with this email already exist" });
+      }
+      // securing password from attacker
+      const salt = await bcrypt.genSalt(10);
+      const securePassword = await bcrypt.hash(req.body.password,salt);
+      // creating a user
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password:req.body.password,
-      }).then(user => res.json(user));
-})
+        password: securePassword,
+      });
 
-module.exports = router
+      res.json(user);
+    } 
+    catch (error) {
+      console.error(error.message);
+      res.status(500).send("Somethind happening wrong");
+    }
+    // .then(user => res.json(user))
+    // .catch(err =>{
+    //   res.json({error: "Email already exist!"})
+    // });
+  }
+);
+
+module.exports = router;
